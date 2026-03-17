9f3fd26c30d196ccc7832c24da286c392d650c70
todocodeacademy <admin@todocodeacademy.com>
2026-03-17 12:18:38 -0300

    Repositorio actualización Spring Boot 4

---
+++ CASO 1 - Versión 2.6.6 + JDK 11/src/main/java/com/todocodeacademy/helloWorld/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.todocodeacademy.helloWorld.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    
+    @GetMapping
+    public String sayHello() {
+        return "Hola Mundo! Esto es una prueba";
+    }
+    
+}
+
